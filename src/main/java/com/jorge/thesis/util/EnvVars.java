bd3ab04ddb84cf70e74ec7dbdb9313cb86888d95
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-12-28 21:12:02 +0100

    Fixes

--- src/main/java/com/jorge/thesis/util/EnvVars.java
+++ src/main/java/com/jorge/thesis/util/EnvVars.java
@@ -2,8 +2,8 @@ package com.jorge.thesis.util;
 
 public final class EnvVars {
 
-    public static final String API_KEY = System.getenv("COMM_EXP_API_KEY");
-    public static final String PORT = System.getenv("COMM_EXP_PORT");
+    public static final String API_KEY = System.getenv("COMM_EXP_GCM_SERV_API_KEY");
+    public static final String PORT = System.getenv("COMM_EXP_GCM_SERV_PORT");
 
     private EnvVars() throws IllegalAccessException {
         throw new IllegalAccessException("DO NOT CONSTRUCT " + EnvVars.class.getName());
